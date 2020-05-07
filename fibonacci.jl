3a07d97a22954be114f9fd6c01d7be8b492b3263
Ben Vest <bvest@innervent.net>
2020-05-06 23:16:18 -0500

    jupin

--- fibonacci.jl
+++ fibonacci.jl
@@ -8,31 +8,13 @@ function fibonacci(n)
   u0 = [ 0 ; 1 ]
   A = [ 1 1 ; 1 0 ]
   # Solve[-x^2+x-1 == 0]
-  lambda_1 = (1 + sqrt(5))/2
-  lambda_2 = (1 - sqrt(5))/2
+  lambda_1 = (1 + sqrt(big(5)))/2
+  lambda_2 = (1 - sqrt(big(5)))/2
   S = [ lambda_1 lambda_2 ; 1 1 ]
   D = [ lambda_1 0 ; 0 lambda_2 ]
   # u0 = Sc => c = S^-1 u0
   c = inv(S)*u0
-  try
-    return Int(round(last(S*D^n*c)))
-  catch err
-    if isa(err, InexactError)
-      return round(last(S*D^n*c))
-    end
-  end
-end
-
-function generate(from, to)
-  fibs = []
-  for j = from:to
-    push!(fibs, fibonacci(j))
-  end
-  return fibs
-end
-
-function generate(n)
-  return generate(1, n)
+  return BigInt(round(last(S*D^n*c)))
 end
 
 function recursive_fibonacci(n)
@@ -65,4 +47,18 @@ function memfib(n)
       end
     end
   end
+end
+
+# helper generators
+
+function generate(from, to)
+  fibs = []
+  for j = from:to
+    push!(fibs, fibonacci(j))
+  end
+  return fibs
+end
+
+function generate(n)
+  return generate(1, n)
 end
